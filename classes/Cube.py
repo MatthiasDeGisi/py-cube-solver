15ea19298a0a695cfbfde215f680f157bad78565
Matthias De Gisi <matthias.degisi@gmail.com>
2023-12-23 01:14:01 -0800

    added f turn

--- classes/Cube.py
+++ classes/Cube.py
@@ -20,6 +20,15 @@ class Cube:
         self.__bottom = [[Y, Y, Y], [Y, Y, Y], [Y, Y, Y]]
         self.__right = [[R, R, R], [R, R, R], [R, R, R]]
         self.__left = [[O, O, O], [O, O, O], [O, O, O]]
+        # side mapping for getters and setters
+        self.side_mapping = {
+            "front": self.__front,
+            "back": self.__back,
+            "top": self.__top,
+            "bottom": self.__bottom,
+            "right": self.__right,
+            "left": self.__left,
+        }
 
     def get_side(self, side: str) -> list:
         """Return the array representation of a side.
@@ -30,18 +39,38 @@ class Cube:
         Returns:
             list: the representation of the side
         """
-        if side == "front":
-            return self.__front
-        if side == "back":
-            return self.__back
-        if side == "top":
-            return self.__top
-        if side == "bottom":
-            return self.__bottom
-        if side == "right":
-            return self.__right
-        if side == "left":
-            return self.__left
+        return self.side_mapping[side]
+
+    def f_turn(self) -> None:
+        """Turn the front side of the cube."""
+
+        # Save the original states for reference
+        original_front = [row[:] for row in self.__front]
+        original_top = [row[:] for row in self.__top]
+        original_right = [row[:] for row in self.__right]
+        original_bottom = [row[:] for row in self.__bottom]
+        original_left = [row[:] for row in self.__left]
+
+        # Turn front side
+        for i in range(3):
+            for j in range(3):
+                self.__front[j][2 - i] = original_front[i][j]
+
+        # Update top side
+        for i in range(3):
+            self.__top[2][i] = original_left[2 - i][2]
+
+        # Update right side
+        for i in range(3):
+            self.__right[i][0] = original_top[2][i]
+
+        # Update bottom side
+        for i in range(3):
+            self.__bottom[0][i] = original_right[i][0]
+
+        # Update left side
+        for i in range(3):
+            self.__left[i][2] = original_bottom[0][i]
 
     def __str__(self) -> str:
         """Return a visual representation of the cube.
@@ -63,7 +92,7 @@ class Cube:
                 cube_string += item + " "
             for item in self.get_side("front")[row]:
                 cube_string += item + " "
-            for item in self.get_side("left")[row]:
+            for item in self.get_side("right")[row]:
                 cube_string += item + " "
             for item in self.get_side("back")[row]:
                 cube_string += item + " "
--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import classes.Cube as c
 
 cube1 = c.Cube()
 print(cube1)
+cube1.f_turn()
+print(cube1)
