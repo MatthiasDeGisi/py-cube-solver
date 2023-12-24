17ecdeb6c11679b37da4be3d77ffb9b6bfed9f0a
Matthias De Gisi <matthias.degisi@gmail.com>
2023-12-24 00:24:48 -0800

    divided main into main and test

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
+import os
+import time
+from sty import fg
 import classes.Cube as c
 
+os.system("cls")
 cube1 = c.Cube()
-print(cube1)
-# #front turn and rev
-# cube1.f_turn()
-# print(cube1)
-# cube1.f_turn_prime()
-# print(cube1)
-# # b turn and rev
-# cube1.b_turn()
-# print(cube1)
-# cube1.b_turn_prime()
-# print(cube1)
+while True:
+    print(f"\n{cube1}")
+    user_input = input(fg.rs + "Turn (h for help): ")
+    if user_input.lower() == "h":
+        print(
+            "Clockwise:\nF = front\nB = back\nT = top\nD = bottom\nL = left\nR = right\nCounterclockwise:\nAppend ' to input."
+        )
+        input("Press any key to continue.")
+        
+    elif user_input.lower() == "f":
+        cube1.f_turn()
+        
+    elif user_input.lower() == "f'":
+        cube1.f_turn_prime()
+        
+    elif user_input.lower() == "b":
+        cube1.b_turn()
+        
+    elif user_input.lower() == "b'":
+        cube1.b_turn_prime()
+        
+    else:
+        print("Input not recognized.")
---
+++ test.py
@@ -0,0 +1,14 @@
+import classes.Cube as c
+
+cube1 = c.Cube()
+print(cube1)
+# #front turn and rev
+# cube1.f_turn()
+# print(cube1)
+# cube1.f_turn_prime()
+# print(cube1)
+# # b turn and rev
+# cube1.b_turn()
+# print(cube1)
+# cube1.b_turn_prime()
+# print(cube1)
