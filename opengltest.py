a111a5e2247c53649aa30229efb7c90005d68b36
Matthias De Gisi <matthias.degisi@gmail.com>
2023-12-27 15:18:52 -0800

    optimized the front()

--- opengltest.py
+++ opengltest.py
@@ -1,61 +1,50 @@
 import classes.Cube as c
-
 import pygame
 from pygame.locals import *
-
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-verticies = (
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1),
-    # # From me 8v
-    # (-.33, 1, 1),
-    # (0.33, 1, 1),
-    # (-.33, -1, 1),
-    # (0.33, -1, 1),
-    # #12v
-    # (-1, -.33, 1),
-    # (-1, 0.33, 1),
-    # (1, -.33, 1),
-    # (1, 0.33, 1),
-)
+# verticies = (
+#     (1, -1, -1),
+#     (1, 1, -1),
+#     (-1, 1, -1),
+#     (-1, -1, -1),
+#     (1, -1, 1),
+#     (1, 1, 1),
+#     (-1, -1, 1),
+#     (-1, 1, 1),
+#     # # From me 8v
+#     # (-.33, 1, 1),
+#     # (0.33, 1, 1),
+#     # (-.33, -1, 1),
+#     # (0.33, -1, 1),
+#     # #12v
+#     # (-1, -.33, 1),
+#     # (-1, 0.33, 1),
+#     # (1, -.33, 1),
+#     # (1, 0.33, 1),
+# )
 
-edges = (
-    (0, 1),
-    (0, 3),
-    (0, 4),
-    (2, 1),
-    (2, 3),
-    (2, 7),
-    (6, 3),
-    (6, 4),
-    (6, 7),
-    (5, 1),
-    (5, 4),
-    (5, 7),
-    # # from me
-    # (8, 10),
-    # (9, 11),
-    # (12, 14),
-    # (13, 15),
-)
+# edges = (
+#     (0, 1),
+#     (0, 3),
+#     (0, 4),
+#     (2, 1),
+#     (2, 3),
+#     (2, 7),
+#     (6, 3),
+#     (6, 4),
+#     (6, 7),
+#     (5, 1),
+#     (5, 4),
+#     (5, 7),
+#     # # from me
+#     # (8, 10),
+#     # (9, 11),
+#     # (12, 14),
+#     # (13, 15),
+# )
 
-colors = (
-    (1, 0, 0),  # Red
-    (0, 1, 0),  # Green
-    (0, 0, 1),  # Blue
-    (1, 1, 0),  # Yellow
-    (1, 0, 1),  # Magenta
-    (0, 1, 1),  # Cyan
-    (1, 1, 1),  # White
-)
 
 
 # def Cube():
@@ -79,63 +68,42 @@ colors = (
 #     glVertex3fv((1, 0, 0))
 #     glVertex3fv((0, 0, 0))
 #     glEnd()
+colors = (
+    (1, 0, 0),  # Red
+    (0, 1, 0),  # Green
+    (0, 0, 1),  # Blue
+    (1, 1, 0),  # Yellow
+    (1, 0, 1),  # Magenta
+    (0, 1, 1),  # Cyan
+    (1, 1, 1),  # White
+)
     
 def Front():
     glBegin(GL_QUADS)
-    # Up Left
-    glColor3fv(colors[0])
-    glVertex3fv((-1, 1, 1))
-    glVertex3fv((-.33, 1, 1))
-    glVertex3fv((-.33, .33, 1))
-    glVertex3fv((-1, .33, 1))
-    # Up Middle
-    glColor3fv(colors[1])
-    glVertex3fv((-.33, 1, 1))
-    glVertex3fv((.33, 1, 1))
-    glVertex3fv((.33, .33, 1))
-    glVertex3fv((-.33, .33, 1))
-    # Up Right
-    glColor3fv(colors[2])
-    glVertex3fv((.33, 1, 1))
-    glVertex3fv((1, 1, 1))
-    glVertex3fv((1, .33, 1))
-    glVertex3fv((.33, .33, 1))
-    # Middle Left
-    glColor3fv(colors[3])
-    glVertex3fv((-1, .33, 1))
-    glVertex3fv((-.33, .33, 1))
-    glVertex3fv((-.33, -.33, 1))
-    glVertex3fv((-1, -.33, 1))
-    # Middle Middle
-    glColor3fv(colors[4])
-    glVertex3fv((-.33, .33, 1))
-    glVertex3fv((.33, .33, 1))
-    glVertex3fv((.33, -.33, 1))
-    glVertex3fv((-.33, -.33, 1))
-    # Middle Right
-    glColor3fv(colors[5])
-    glVertex3fv((.33, .33, 1))
-    glVertex3fv((1, .33, 1))
-    glVertex3fv((1, -.33, 1))
-    glVertex3fv((.33, -.33, 1))
-    # Down Left
-    glColor3fv(colors[6])
-    glVertex3fv((-1, -.33, 1))
-    glVertex3fv((-.33, -.33, 1))
-    glVertex3fv((-.33, -1, 1))
-    glVertex3fv((-1, -1, 1))
-    # Down Middle
-    glColor3fv(colors[0])
-    glVertex3fv((-.33, -.33, 1))
-    glVertex3fv((.33, -.33, 1))
-    glVertex3fv((.33, -1, 1))
-    glVertex3fv((-.33, -1, 1))
-    # Down Right
-    glColor3fv(colors[1])
-    glVertex3fv((.33, -.33, 1))
-    glVertex3fv((1, -.33, 1))
-    glVertex3fv((1, -1, 1))
-    glVertex3fv((.33, -1, 1))
+    
+    parts = [
+        ((-1, 1), (-.33, 1), (-.33, .33), (-1, .33)),  # Up Left
+        ((-.33, 1), (.33, 1), (.33, .33), (-.33, .33)),  # Up Middle
+        ((.33, 1), (1, 1), (1, .33), (.33, .33)),  # Up Right
+        ((-1, .33), (-.33, .33), (-.33, -.33), (-1, -.33)),  # Middle Left
+        ((-.33, .33), (.33, .33), (.33, -.33), (-.33, -.33)),  # Middle Middle
+        ((.33, .33), (1, .33), (1, -.33), (.33, -.33)),  # Middle Right
+        ((-1, -.33), (-.33, -.33), (-.33, -1), (-1, -1)),  # Down Left
+        ((-.33, -.33), (.33, -.33), (.33, -1), (-.33, -1)),  # Down Middle
+        ((.33, -.33), (1, -.33), (1, -1), (.33, -1)),  # Down Right
+    ]
+
+    colors = [
+        (1, 0, 0), (0, 1, 0), (0, 0, 1),  # Replace with your specific colors
+        (1, 1, 0), (1, 0, 1), (0, 1, 1),
+        (1, 1, 1), (1, 0, 0), (0, 1, 0),
+    ]
+
+    for i, part in enumerate(parts):
+        glColor3fv(colors[i])
+        for vertex in part:
+            glVertex3fv((*vertex, 1))
+
     glEnd()
     
 
